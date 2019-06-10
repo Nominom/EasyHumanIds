2e6155322fec823178197ea23608e89801800a9c
Nominom <nomi.lakkala@gmail.com>
2019-06-10 10:26:30 +0300

    Add project files.

---
+++ EasyHumanIds-Standard/Library.fs
@@ -0,0 +1,13 @@
+﻿namespace EasyHumanIds
+
+open RandomWords
+
+module HumanIds =
+
+    let private takeOneRandom (rnd : System.Random) (list : _ array)  = 
+        list.[rnd.Next(0, list.Length)]
+
+    let Generate (rnd : System.Random) =
+        let adjective = takeOneRandom rnd RandomWords.adjectives 
+        let noun = takeOneRandom rnd RandomWords.nouns 
+        adjective + "-" + noun
---
+++ TestingSuite/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EasyHumanIds;
+
+namespace TestingSuite
+{
+	class Program
+	{
+		static void Main (string[] args) {
+			Console.WriteLine("Hello World!");
+			Random r = new System.Random();
+
+			for(int i=0; i< 100;i++) {
+				Console.WriteLine(HumanIds.Generate(r));
+			}
+		}
+	}
+}
